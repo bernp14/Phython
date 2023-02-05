be4bdd39305077df4dda731e8148de923726002b
Bernard Plaire <bplaire@hotmail.com>
2023-02-05 15:45:42 +0100

    adaptation minime pour preciser comment utilsier ce fichier en ligne de commande

--- tk.py
+++ tk.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 fen1 = Tk()
-text1 = Label(fen1, text='Bonjour tout le monde !', fg='red')
+text1 = Label(fen1, text='Bonjour tout le monde, executez moi avec la command python -m tk !', fg='red')
 text1.pack()
 
 bou1 = Button(fen1, text='Quitter', command =fen1.destroy)
